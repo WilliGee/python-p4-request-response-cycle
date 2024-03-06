2f3f5723b58e30040cd444f98790c1f6555b6315
Wilson Gachanja <wilson.gachanja@student.moringaschool.com>
2024-03-06 22:47:53 +0300

    Completed Assignment

--- server/app.py
+++ server/app.py
@@ -4,5 +4,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.router('/')
+def index():
+    host = request.headers.get('HOST')
+    return f'<h1>The host for this page is {host}</h1>'
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
